16451cc03a3d84e2cdcde5fcb0fbeb7b9521f50c
Igor <zakatov0303@gmail.com>
2026-08-06 21:38:45 +0300

    Add validation to send submission endpoint

---
+++ backend/src/main/kotlin/com/codeloom/backend/dto/SendSubmissionRequest.kt
@@ -0,0 +1,15 @@
+package com.codeloom.backend.dto
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+
+data class SendSubmissionRequest(
+    @NotNull
+    val problemId: Long,
+
+    @NotBlank
+    val code: String,
+
+    @NotBlank
+    val language: String,
+)
